df7f4d6b7bac7373b48eebf021cacc8d0e2b0bc6
GWilczyk <gwilczyk.github@gmail.com>
2022-10-07 11:21:10 +0200

    Standard middleware & Security middleware

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,4 +1,10 @@
-import { Application } from 'express'
+import compression from 'compression'
+import cookieSession from 'cookie-session'
+import cors from 'cors'
+import { Application, json, urlencoded } from 'express'
+import 'express-async-errors'
+import helmet from 'helmet'
+import hpp from 'hpp'
 import { Server } from 'http'
 
 export class WannaChatServer {
@@ -16,9 +22,32 @@ export class WannaChatServer {
 		this.startServer(this.app)
 	}
 
-	private securityMiddleware(app: Application): void {}
+	private securityMiddleware(app: Application): void {
+		app.use(
+			cookieSession({
+				name: 'session',
+				keys: ['test1', 'test2'],
+				maxAge: 7 * 24 * 3600000 /* 7 days */,
+				secure: false,
+			})
+		)
+		app.use(hpp())
+		app.use(helmet())
+		app.use(
+			cors({
+				origin: '*',
+				credentials: true,
+				optionsSuccessStatus: 200,
+				methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+			})
+		)
+	}
 
-	private standardMiddleware(app: Application): void {}
+	private standardMiddleware(app: Application): void {
+		app.use(compression())
+		app.use(json({ limit: '50mb' }))
+		app.use(urlencoded({ extended: true, limit: '50mb' }))
+	}
 
 	private routesMiddleware(app: Application): void {}
 
