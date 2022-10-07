4709e71e3faac276cb56f4b7a14a465368006029
GWilczyk <gwilczyk.github@gmail.com>
2022-10-07 19:01:06 +0200

    Auth interface and model

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb';
+import { Document } from 'mongoose';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload;
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  uId: string;
+  email: string;
+  username: string;
+  avatarColor: string;
+  iat?: number;
+}
+
+export interface IAuthDocument extends Document {
+  _id: string | ObjectId;
+  uId: string;
+  username: string;
+  email: string;
+  password?: string;
+  avatarColor: string;
+  createdAt: Date;
+  comparePassword(password: string): Promise<boolean>;
+  hashPassword(password: string): Promise<string>;
+}
+
+export interface ISignupData {
+  _id: ObjectId;
+  uId: string;
+  email: string;
+  username: string;
+  password: string;
+  avatarColor: string;
+}
+
+export interface IAuthJob {
+  value?: string | IAuthDocument;
+}
