ef37496107730bc02022a489902fd1887363ab5c
GWilczyk <gwilczyk.github@gmail.com>
2022-10-07 11:57:10 +0200

    Database connection setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from 'express'
+import databaseConnection from './setupDatabase'
 import { WannaChatServer } from './setupServer'
 
 class Application {
 	public initialize(): void {
+		databaseConnection()
 		const app: Express = express()
 		const server: WannaChatServer = new WannaChatServer(app)
 		server.start()
