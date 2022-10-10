ff786779c3323251de794c9ebc65d12a790fde4a
GWilczyk <gwilczyk.github@gmail.com>
2022-10-10 11:21:08 +0200

    User logout controller

--- src/features/auth/routes/auth.routes.ts
+++ src/features/auth/routes/auth.routes.ts
@@ -1,4 +1,5 @@
 import { Signin } from '@auth/controllers/signin.controller';
+import { Signout } from '@auth/controllers/signout.controller';
 import { Signup } from '@auth/controllers/signup.controller';
 import express, { Router } from 'express';
 
@@ -12,6 +13,11 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', Signup.prototype.create);
     this.router.post('/signin', Signin.prototype.read);
+    return this.router;
+  }
+
+  public signoutRoute(): Router {
+    this.router.get('/signout', Signout.prototype.update);
     return this.router;
   }
 }
