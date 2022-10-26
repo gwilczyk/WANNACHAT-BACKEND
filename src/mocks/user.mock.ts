0d558e0373d04dcdc0849f41a175f1ada38513f1
GWilczyk <gwilczyk.github@gmail.com>
2022-10-26 18:48:35 +0200

    Current user unit tests

--- src/mocks/user.mock.ts
+++ src/mocks/user.mock.ts
@@ -1,31 +1,142 @@
 import { IUserDocument } from '@user/interfaces/user.interface';
 
-export const mergedAuthAndUserData: IUserDocument = {
-  _id: '605727cd646eb50e668a4e13',
-  authId: '60263f14648fed5246e322d3',
-  username: 'Jane',
-  email: 'jane@mail.com',
-  password: 'P4ssword',
-  avatarColor: '#9c27b0',
-  uId: '1621613119252066',
-  postsCount: 0,
-  work: '',
-  school: '',
-  quote: '',
-  location: '',
+export const mockExistingUser = {
+  notifications: {
+    messages: true,
+    reactions: true,
+    comments: true,
+    follows: true
+  },
+  social: {
+    facebook: '',
+    instagram: '',
+    twitter: '',
+    youtube: ''
+  },
   blocked: [],
   blockedBy: [],
-  followersCount: 0,
-  followingCount: 0,
-  notifications: { messages: true, reactions: true, comments: true, follows: true },
-  social: { facebook: '', instagram: '', twitter: '', youtube: '' },
+  followersCount: 1,
+  followingCount: 2,
+  postsCount: 2,
   bgImageVersion: '',
   bgImageId: '',
-  profilePicture: 'https://res.cloudinary.com/ratingapp/image/upload/605727cd646eb50e668a4e13',
-  createdAt: '2022-08-31T07:42:24.451Z'
-  // birthday: { month: '', day: '' },
-  // gender: '',
-  // about: '',
-  // relationship: '',
-  // placesLived: [],
+  profilePicture: 'http://place-hold.it/500x500',
+  _id: '60263f14648fed5246e322d9',
+  work: 'KickChat Inc.',
+  school: 'University of Benin',
+  location: 'Dusseldorf, Germany',
+  quote: 'Sky is my limit',
+  createdAt: new Date()
 } as unknown as IUserDocument;
+
+export const existingUser = {
+  notifications: {
+    messages: true,
+    reactions: true,
+    comments: true,
+    follows: true
+  },
+  social: {
+    facebook: '',
+    instagram: '',
+    twitter: '',
+    youtube: ''
+  },
+  blocked: [],
+  blockedBy: [],
+  followersCount: 1,
+  followingCount: 2,
+  postsCount: 2,
+  bgImageVersion: '',
+  bgImageId: '',
+  profilePicture: 'http://place-hold.it/500x500',
+  _id: '60263f14648fed5246e322d9',
+  uId: '1621613119252066',
+  username: 'Jane',
+  email: 'jane@mail.com',
+  avatarColor: 'red',
+  work: 'KickChat Inc.',
+  school: 'University of Benin',
+  location: 'Dusseldorf, Germany',
+  quote: 'Sky is my limit',
+  createdAt: new Date()
+} as unknown as IUserDocument;
+
+export const existingUserTwo = {
+  notifications: {
+    messages: false,
+    reactions: true,
+    comments: true,
+    follows: false
+  },
+  social: {
+    facebook: '',
+    instagram: '',
+    twitter: '',
+    youtube: ''
+  },
+  blocked: [],
+  blockedBy: [],
+  followersCount: 1,
+  followingCount: 2,
+  postsCount: 2,
+  bgImageVersion: '',
+  bgImageId: '',
+  profilePicture: 'http://place-hold.it/500x500',
+  _id: '60263f14648fed5246e322d8',
+  uId: '1621613119252065',
+  username: 'Danny',
+  email: 'danny@me.com',
+  avatarColor: '#9c27b1',
+  work: 'KickChat Inc.',
+  school: 'University of Benin',
+  location: 'Dusseldorf, Germany',
+  quote: 'Sky is my limit',
+  createdAt: new Date()
+} as unknown as IUserDocument;
+
+export const mergedAuthAndUserData = {
+  notifications: {
+    messages: false,
+    reactions: true,
+    comments: true,
+    follows: false
+  },
+  social: {
+    facebook: '',
+    instagram: '',
+    twitter: '',
+    youtube: ''
+  },
+  blocked: [],
+  blockedBy: [],
+  followersCount: 1,
+  followingCount: 2,
+  postsCount: 2,
+  bgImageVersion: '',
+  bgImageId: '',
+  profilePicture: 'http://place-hold.it/500x500',
+  _id: '60263f14648fed5246e322d8',
+  authId: '60263f14648fed5246e322d3',
+  uId: '1621613119252066',
+  username: 'Jane',
+  email: 'jane@mail.com',
+  avatarColor: '#9c27b0',
+  work: 'KickChat Inc.',
+  school: 'University of Benin',
+  location: 'Dusseldorf, Germany',
+  quote: 'Sky is my limit',
+  createdAt: '2022-08-31T07:42:24.451Z'
+} as unknown as IUserDocument;
+
+export const searchedUserMock = {
+  profilePicture: 'http://place-hold.it/500x500',
+  _id: '60263f14648fed5246e322d5',
+  uId: '1621613119252062',
+  username: 'Kenny',
+  email: 'ken@me.com',
+  avatarColor: '#9c27b1'
+};
+
+export const userJwt =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
