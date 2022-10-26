25bf6eac6a11abc62d3f165bb43d8a52b9c38d82
GWilczyk <gwilczyk.github@gmail.com>
2022-10-26 21:30:17 +0200

    Create post controller method

---
+++ src/features/posts/controllers/createPost.controller.ts
@@ -0,0 +1,34 @@
+import { joiValidation } from '@globals/decorators/joi-validation.decorators';
+import { IPostDocument } from '@posts/interfaces/posts.interfaces';
+import { postSchema } from '@posts/schemes/posts.scheme';
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { ObjectId } from 'mongodb';
+
+export class Create {
+  @joiValidation(postSchema)
+  public async post(req: Request, res: Response): Promise<void> {
+    const { post, bgColor, privacy, feelings, gifUrl, profilePicture } = req.body;
+    const postObjectId: ObjectId = new ObjectId();
+    const createdPost: IPostDocument = {
+      _id: postObjectId,
+      userId: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      email: req.currentUser!.email,
+      avatarColor: req.currentUser!.avatarColor,
+      profilePicture,
+      post,
+      bgColor,
+      feelings,
+      gifUrl,
+      privacy,
+      commentsCount: 0,
+      imgVersion: '',
+      imgId: '',
+      createdAt: new Date(),
+      reactions: { like: 0, love: 0, happy: 0, wow: 0, sad: 0, angry: 0 }
+    } as IPostDocument;
+
+    res.status(HTTP_STATUS.CREATED).json({ message: 'Post created successfully' });
+  }
+}
