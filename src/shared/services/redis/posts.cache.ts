d89e429e38b8ed02431375d36c99ae1bb0b3775b
GWilczyk <gwilczyk.github@gmail.com>
2022-10-29 16:21:24 +0200

    Get posts from Redis cache

--- src/shared/services/redis/posts.cache.ts
+++ src/shared/services/redis/posts.cache.ts
@@ -1,14 +1,47 @@
 import { ServerError } from '@globals/helpers/error-handler';
-import { ISavePostToCache } from '@posts/interfaces/posts.interfaces';
+import { Helpers } from '@globals/helpers/helpers';
+import { IPostDocument, ISavePostToCache } from '@posts/interfaces/posts.interfaces';
+import { IReactions } from '@reactions/interfaces/reactions.interfaces';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
 import { config } from '@root/config';
 import { BaseCache } from '@services/redis/base.cache';
 import Logger from 'bunyan';
 
 const log: Logger = config.createLogger('postsCache');
 
+export type PostsCacheMultiType = string | number | Buffer | RedisCommandRawReply[] | IPostDocument | IPostDocument[];
+
 export class PostsCache extends BaseCache {
   constructor() {
     super('postsCache');
+  }
+
+  public async getPostsFromCache(key: string, start: number, end: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, start, end, { REV: true });
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for (const value of reply) {
+        multi.HGETALL(`posts:${value}`);
+      }
+      const replies: PostsCacheMultiType = (await multi.exec()) as PostsCacheMultiType;
+      const postReplies: IPostDocument[] = [];
+      for (const post of replies as IPostDocument[]) {
+        post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+        post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+        post.createdAt = new Date(Helpers.parseJson(`${post.createdAt}`)) as Date;
+
+        postReplies.push(post);
+      }
+
+      return postReplies;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
   }
 
   public async savePostToCache(data: ISavePostToCache): Promise<void> {
